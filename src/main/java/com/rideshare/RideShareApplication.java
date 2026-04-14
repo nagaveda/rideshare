78bfd0d277b399486d2018ef16fb9b788ef33d28
nagaveda <nagaveda.redddy@gmail.com>
2026-04-14 00:00:08 -0700

    Scaffolding initial springboot project along with infra setup - kafka/redis/postgres via docker

---
+++ src/main/java/com/rideshare/RideShareApplication.java
@@ -0,0 +1,14 @@
+package com.rideshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class RideShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RideShareApplication.class, args);
+    }
+}
